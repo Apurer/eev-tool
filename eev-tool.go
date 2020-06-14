29f5e92231aaf0682d7833b6fdaa6d18cc84ff36
Maciej <maciej@contact.apurer.io>
2020-06-15 00:37:26 +0200

    draft

--- eev-tool.go
+++ eev-tool.go
@@ -1,8 +1,21 @@
 package main 
 
 import (
-
+	"golang.org/x/crypto/ssh/terminal"
+	"runtime"
+	"syscall"
+	"strings"
+	"bufio"
+	"flag"
+    "fmt"
+    "os"
 )
+
+// all values will have to be passed here 
+
+func InteractiveMode() {
+
+}
 
 func main() {
 /* 
@@ -15,5 +28,53 @@ func main() {
 	two ways of running script: interactive or non interactive mode
 	interactive mode is when important values are not defined using parameters or just no parameters are passed at all
 	non interactive mode is when all important values are passed on input
+	if one of the values will not get passed program will turn into interactive mode and ask user for this value
+
+	each step will get values and based on passed or not passed value it will continue in interactive or non interactive mode
+
+	maybe at start owkr only with bash_profile and env variables instead of creating directory
+	read set in env 
+	by default copy to clipboard - instead of showing output
+	EEV_clipboard - boolean
+	EEV_privatekey - path to privatekey
+	other env variables even those encrypted can be set with normal names
+	don't modify files for now but put generated private keys where use wants
+
+	on interactive mode with no parameters passed give user options on what to do
+	- generate private key
+	- encrypt value - by default using private key set in env variables
+	- decrypt value - in interactive mode user can choose if he wants to copy value to clipboard - if value in env variable is not set
+	- decrypting value can be done by providing value or name of env variable - give user option
+		- decrypt value
+		- decrypt env variable
+			- copy value to clipboard
+			- print output in terminal
 */	
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter Username: ")
+	username, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter Password: ")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err == nil {
+		fmt.Println("\nPassword typed: " + string(bytePassword))
+		fmt.Println(int(syscall.Stdin))
+	}
+	password := string(bytePassword)
+
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	case "windows":
+		fmt.Println("Windows.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+}
 }
