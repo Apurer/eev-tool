315737e7980544ab519b1669974925a5fe605c09
Maciej <maciej@contact.apurer.io>
2020-06-21 04:22:05 +0200

    working test version

--- eev-tool.go
+++ eev-tool.go
@@ -3,10 +3,14 @@ package main
 import (
 	privateKey "github.com/Apurer/eev/privatekey"
 	"golang.org/x/crypto/ssh/terminal"
+	AES "github.com/Apurer/eev/aes"
+	"encoding/pem"
 	"crypto/x509"
-	"runtime"
+	//"runtime"
+	"io/ioutil"
 	"syscall"
-	"strings"
+	"bytes"
+	//"strings"
 	"bufio"
 	"flag"
 	"fmt"
@@ -14,11 +18,6 @@ import (
     "os"
 )
 
-// all values will have to be passed here 
-
-func InteractiveMode() {
-
-}
 /* 
 	generates private key
 	generates encrypted value and puts it into the home directory ~/.eev
@@ -78,7 +77,8 @@ func InteractiveMode() {
 		- for private key generates element in passed path returning 0 for given line or empty line
 		- for encrypted value outputs decrypted value in terminal
 		- for encrypting outputs encrypted value in terminal
-*/	
+*/
+
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
@@ -102,62 +102,65 @@ func main() {
 
 	flag.Parse()
 
-	if keytype != "" || keysize != 0 || alg != "" {
+	if *keytype != "" || *keysize != 0 || *alg != "" {
 		// creating private key
-		for interactive && !(keytype ==  "RSA" || keytype ==  "ECDSA") {
+		for *interactive && !(*keytype ==  "RSA" || *keytype ==  "ECDSA") {
 			// prompt
 			fmt.Println("This is a list of private key types to choose from:")
 			fmt.Println("RSA")
 			fmt.Println("ECDSA")
 			fmt.Println("Please provide type of private key.")
-			fmt.Fscan(reader, &keytype)
+			fmt.Fscan(reader, keytype)
+
 		}
 
-		if !interactive && !(keytype ==  "RSA" || keytype ==  "ECDSA") {
+		if !*interactive && !(*keytype ==  "RSA" || *keytype ==  "ECDSA") {
 			log.Fatal("Type of private key does not fit available values.\nAvailable types:\nRSA\nECDSA")
 		}
 
-		for interactive && keytype ==  "RSA" && !(keysize == 128 || keysize == 192 || keysize == 256)  {
+		for *interactive && *keytype ==  "RSA" && !(*keysize == 128 || *keysize == 192 || *keysize == 256)  {
 			// prompt
 			fmt.Println("This is a list of RSA private key sizes to choose from:")
 			fmt.Println("128")
 			fmt.Println("192")
 			fmt.Println("256")
 			fmt.Println("Please provide size of private key: ")
-			fmt.Fscan(reader, &keysize)
+			//var keySize int
+			fmt.Fscan(reader, keysize)
+			//*keysize = keySize
 		}
 
-		if !interactive && keytype ==  "RSA" && !(keysize == 128 || keysize == 192 || keysize == 256) {
+		if !*interactive && *keytype ==  "RSA" && !(*keysize == 128 || *keysize == 192 || *keysize == 256) {
 			log.Fatalf("Size: %d of private key does not fit available values for RSA type.\nAvailable sizes for RSA:\n128\n192\n256\n", keysize)
 		}
 
-		for interactive && keytype ==  "ECDSA" && !(keysize == 256) {
+		for *interactive && *keytype ==  "ECDSA" && !(*keysize == 256) {
 			// prompt
 			fmt.Println("This is a list of ECDSA private key sizes to choose from:")
 			fmt.Println("256")
 			fmt.Println("Please provide size of private key.")
-			fmt.Fscan(reader, &keysize)
+			fmt.Fscan(reader, keysize)
 		}
 
-		if !interactive && keytype ==  "ECDSA" && !(keysize == 256) {
+		if !*interactive && *keytype ==  "ECDSA" && !(*keysize == 256) {
 			log.Fatalf("Size: %d of private key does not fit available values for ECDSA type.\nAvailable sizes for ECDSA:\n256\n", keysize)
 		}
 
-		for interactive && !(alg == "AES128" || alg == "AES192" || alg == "AES256") {
+		for *interactive && !(*alg == "AES128" || *alg == "AES192" || *alg == "AES256" || *alg == "") {
 			// prompt
 			fmt.Println("This is a list of private key encryption algorithms to choose from:")
 			fmt.Println("AES128")
 			fmt.Println("AES192")
 			fmt.Println("AES256")
 			fmt.Println("Please provide correct algorithm for private key encryption.")
-			fmt.Fscan(reader, &alg)
+			fmt.Fscan(reader, alg)
 		}
 
-		if !interactive && !(alg == "AES128" || alg == "AES192" || alg == "AES256") {
+		if !*interactive && !(*alg == "AES128" || *alg == "AES192" || *alg == "AES256") {
 			log.Fatalf("Encryption algorithm: %s of private key does not fit available values.\nAvailable encryption algorithms:\nAES128\nAES192\nAES256", alg)
 		}
 		
-		for interactive && alg != "" && passphrase == "" {
+		for *interactive && *alg != "" && *passphrase == "" {
 			// prompt
 			fmt.Println("Provide passphrase for private key: ")
 			passphrase_bytes, err := terminal.ReadPassword(int(syscall.Stdin))
@@ -169,77 +172,85 @@ func main() {
 			if err != nil {
 				log.Fatal(err, " passphrase input error")
 			}
-			if !bytes.Equal(masterPwd, masterPwd2) {
+			if !bytes.Equal(passphrase_bytes, compare_bytes) {
 				log.Fatal("Passwords do not match. Try again.")
 			} else {
-				passphrase = string(passphrase_bytes)
+				*passphrase = string(passphrase_bytes)
 			}
 		}
 
-		if !interactive && alg != "" && passphrase == "" {
+		if !*interactive && *alg != "" && *passphrase == "" {
 			log.Fatal("Passphrase for private key encryption is empty")
 		}
 
 		var keyType string 
 
-		switch keytype {
+		switch *keytype {
 		case "RSA":
 			keyType = privateKey.RSA
 		case "ECDSA":
 			keyType = privateKey.ECDSA
 		default:
-			log.Fatalf("Unknown private key type: %s.\n", keytype)
+			log.Fatalf("Unknown private key type: %s.\n", *keytype)
 		}
 
-		algorithm := privateKey.NoEncryptionAlgorithm
+		var algorithm x509.PEMCipher
 
-		switch alg {
+		switch *alg {
 		case "AES128":
 			algorithm = privateKey.AES128
 		case "AES192":
 			algorithm = privateKey.AES192
 		case "AES256":
 			algorithm  = privateKey.AES256
+		case "":
+			algorithm = 0
 		default:
-			log.Fatalf("Unknown private key encryption algorithm: %s.\n", alg)
+			log.Fatalf("Unknown private key encryption algorithm: %s.\n", *alg)
 		}
 
-		for interactive && keypath == "" {
+		for *interactive && *keypath == "" {
 			// prompt
 			fmt.Println("Please provide path for private key: ")
-			fmt.Fscan(reader, &keypath)
+			fmt.Fscan(reader, keypath)
 		}
 
-		if !interactive && keypath == "" {
+		if !*interactive && *keypath == "" {
 			log.Fatal("Path for private key encryption is empty")
 		}
 
 		// generate key
-		privkey, err := privateKey.Generate(keyType, keysize)
-		err = privateKey.Write(keypath, privkey, passphrase, algorithm)
+		privkey, err := privateKey.Generate(keyType, *keysize)
+		if err != nil {
+			log.Fatal(err, " error while generating private key")
+		}
+		err = privateKey.Write(*keypath, privkey, *passphrase, algorithm)
+		if err != nil {
+			log.Fatal(err, " error while writing private key to file")
+		} 
 	}
 
-	if decrypt != "" || env_decrypt != "" {
-		if keypath == "" {
-			keypath = os.Getenv("EEV_privatekey")
-			for interactive && keypath == "" {
+	if *decrypt != "" || *env_decrypt != "" {
+		if *keypath == "" {
+			*keypath = os.Getenv("EEV_privatekey")
+			for *interactive && *keypath == "" {
 				// prompt
 				fmt.Println("Please provide path for private key: ")
-				fmt.Fscan(reader, &keypath)
+				fmt.Fscan(reader, keypath)
 			}
 
-			if !interactive && keypath == "" {
+			if !*interactive && *keypath == "" {
 				log.Fatal("Path of private key not set")
 			}
 		}
 
-		privkey, err = ioutil.ReadFile(keypath)
+		privkey, err := ioutil.ReadFile(*keypath)
 		if err != nil {
 			log.Fatal(err, " private key reading error")
 		}
 
 		block, _ := pem.Decode(privkey)
-		if interactive && x509.IsEncryptedPEMBlock(block) && passphrase == "" {
+		if *interactive && x509.IsEncryptedPEMBlock(block) && *passphrase == "" {
 			// prompt
 			fmt.Println("Provide passphrase for private key: ")
 			passphrase_bytes, err := terminal.ReadPassword(int(syscall.Stdin))
@@ -251,14 +262,14 @@ func main() {
 			if err != nil {
 				log.Fatal(err, " passphrase input error")
 			}
-			if !bytes.Equal(masterPwd, masterPwd2) {
+			if !bytes.Equal(passphrase_bytes, compare_bytes) {
 				log.Fatal("Passwords do not match. Try again.")
 			} else {
-				passphrase = string(passphrase_bytes)
+				*passphrase = string(passphrase_bytes)
 			}
 		}
 
-		if !interactive && x509.IsEncryptedPEMBlock(block) && passphrase == "" {
+		if !*interactive && x509.IsEncryptedPEMBlock(block) && *passphrase == "" {
 			log.Fatal("Passphrase for private key decryption is empty")
 		}
 
@@ -267,58 +278,108 @@ func main() {
 			if block == nil {
 				log.Fatal("Decoded pem block is empty")
 			}
-			block.Bytes, err = x509.DecryptPEMBlock(block, []byte(passphrase))
+			block.Bytes, err = x509.DecryptPEMBlock(block, []byte(*passphrase))
 			privkey = pem.EncodeToMemory(block)
 		}
 
-		if env_decrypt != "" {
-			decrypt = os.Getenv(env_decrypt)
+		if *env_decrypt != "" {
+			*decrypt = os.Getenv(*env_decrypt)
 		}
 
-		if decrypt == "" {
-			fmt.Println("Provide value to be decr")
-			fmt.Fscan(reader, &decrypt)
+		if *decrypt == "" {
+			fmt.Println("Provide value to be decrypted")
+			fmt.Fscan(reader, decrypt)
 		}
 		// check if key is 
 		// decrypting value
+		decrypted, err := AES.Decrypt(privkey, *decrypt)
+		if err != nil {
+			log.Fatal(err, "Error during decrypting value")
+		}
+
+		fmt.Println(string(decrypted))
 	}
 
-	if encrypt != "" || env_encrypt != "" {
-		if keypath == "" {
-			keypath = os.Getenv(name)
-			for keypath == "" &&  {
+	if *encrypt != "" || *env_encrypt != "" {
+		if *keypath == "" {
+			*keypath = os.Getenv("EEV_privatekey")
+			for *interactive && *keypath == ""  {
 				// prompt
 				fmt.Println("Please provide path for private key: ")
-				fmt.Fscan(reader, &keypath)
+				fmt.Fscan(reader, keypath)
+			}
+
+			if !*interactive && *keypath == "" {
+				log.Fatal("Path of private key not set")
 			}
 		}
+
+		privkey, err := ioutil.ReadFile(*keypath)
+		if err != nil {
+			log.Fatal(err, " private key reading error")
+		}
+
+		block, _ := pem.Decode(privkey)
+		if *interactive && x509.IsEncryptedPEMBlock(block) && *passphrase == "" {
+			// prompt
+			fmt.Println("Provide passphrase for private key: ")
+			passphrase_bytes, err := terminal.ReadPassword(int(syscall.Stdin))
+			if err != nil {
+				log.Fatal(err, " passphrase input error")
+			}
+			fmt.Println("Enter same passphrase again: ")
+			compare_bytes, err := terminal.ReadPassword(int(syscall.Stdin))
+			if err != nil {
+				log.Fatal(err, " passphrase input error")
+			}
+			if !bytes.Equal(passphrase_bytes, compare_bytes) {
+				log.Fatal("Passwords do not match. Try again.")
+			} else {
+				*passphrase = string(passphrase_bytes)
+			}
+		}
+
+		if !*interactive && x509.IsEncryptedPEMBlock(block) && *passphrase == "" {
+			log.Fatal("Passphrase for private key decryption is empty")
+		}
+
+		if x509.IsEncryptedPEMBlock(block) {
+			block, _ := pem.Decode(privkey)
+			if block == nil {
+				log.Fatal("Decoded pem block is empty")
+			}
+			block.Bytes, err = x509.DecryptPEMBlock(block, []byte(*passphrase))
+			privkey = pem.EncodeToMemory(block)
+		}
 		// encrypting value
+
+		if *env_encrypt != "" {
+			*encrypt = os.Getenv(*env_encrypt)
+		}
+
+		if *encrypt == "" {
+			fmt.Println("Provide value to be decrypted")
+			fmt.Fscan(reader, encrypt)
+		}
+
+		encrypted, err := AES.Encrypt(privkey, *encrypt)
+		if err != nil {
+			log.Fatal(err, "Error during encrypting value")
+		}
+
+		fmt.Println(encrypted)
+
+		// switch os := runtime.GOOS; os {
+		// case "darwin":
+		// 	fmt.Println("OS X.")
+		// case "linux":
+		// 	fmt.Println("Linux.")
+		// case "windows":
+		// 	fmt.Println("Windows.")
+		// default:
+		// 	// freebsd, openbsd,
+		// 	// plan9, windows...
+		// 	fmt.Printf("%s.\n", os)
+		// }
 	}
-
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print("Enter Username: ")
-	username, _ := reader.ReadString('\n')
-
-	fmt.Print("Enter Password: ")
-	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
-	if err == nil {
-		fmt.Println("\nPassword typed: " + string(bytePassword))
-		fmt.Println(int(syscall.Stdin))
-	}
-	password := string(bytePassword)
-
-	switch os := runtime.GOOS; os {
-	case "darwin":
-		fmt.Println("OS X.")
-	case "linux":
-		fmt.Println("Linux.")
-	case "windows":
-		fmt.Println("Windows.")
-	default:
-		// freebsd, openbsd,
-		// plan9, windows...
-		fmt.Printf("%s.\n", os)
-	}
-}
 }
